23fd3eff0865ff7048ea148de8d4c445c8de150c
Victor Sha <kassini_90@mail.ru>
2019-12-01 04:24:47 +0300

    add author selector

--- src/ServiceComponent.js
+++ src/ServiceComponent.js
@@ -1,15 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {Link, useParams} from 'react-router-dom';
 import axios from 'axios';
+import { SERVICE_URL, AUTHORS_URL } from './constants';
+
 
 export const ServiceComponent = () => {
   let {id} = useParams();
   const [service, setService] = useState({});
+  const [authors, setAuthors] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:3004/service/${id}`)
+    axios.get(`${SERVICE_URL}/${id}`)
       .then(res => setService(res.data));
   }, [id]);
+  useEffect(() => {
+    axios.get(AUTHORS_URL)
+      .then(res => setAuthors(res.data));
+  }, [id]);
+
+  const sendRequest = useCallback(async () => {
+    if (!id) {
+      await axios.post(SERVICE_URL, service);
+      return;
+    }
+    await axios.put(`${SERVICE_URL}/${id}`, service);
+  });
+
   console.log(service);
   return (
       <form className="d-flex flex-row justify-content-center">
@@ -21,7 +37,7 @@ export const ServiceComponent = () => {
                   className="form-control"
                   type="text" name="name"
                   value={service.name}
-                  onChange={e => setService({name: e.target.value})}
+                  onChange={e => setService({...service, name: e.target.value})}
                 />
             </label>
           </div>
@@ -34,22 +50,29 @@ export const ServiceComponent = () => {
                 type="number"
                 name="version"
                 value={service.version}
-                onChange={e => setService({version: e.target.value})}
+                onChange={e => setService({...service, version: e.target.value})}
               />
             </label>
           </div>
 
-          <div className="form-group">
-              <label className="col-md-6">
-                  Автор
-                  <input
-                      className="form-control"
-                      type="text"
-                      name="author"
-                      value={service.author_id}
-                      onChange={e => setService({author_id: e.target.value})}
-                  />
-              </label>
+          <div className="form-group pl-3">
+            <label style={{display:"block", width: "345px"}} calssName="d-flex col-md-6" for="author">
+              Автор
+            <select
+              className="form-control "
+              type="text"
+              name="author"
+              id="author"
+              value={service.author_id}
+              onChange={e => setService({...service, author_id: e.target.value})}
+              >
+              {authors.length > 1 &&
+                authors.map(author => (
+                  <option value={author.id}>{author.name}</option>
+                  ))
+                }
+            </select>
+                </label>
           </div>
 
           <div className="form-group">
@@ -60,7 +83,7 @@ export const ServiceComponent = () => {
                       type="textarea"
                       name="description"
                       value={service.description}
-                      onChange={e => setService({description: e.target.value})}
+                      onChange={e => setService({...service, description: e.target.value})}
                   />
               </label>
           </div>
@@ -79,6 +102,24 @@ export const ServiceComponent = () => {
                       <Link to={`/service/${id}`} className="btn btn-primary">Перейти</Link>
                   </div>
               </label>
+          </div>
+
+          <div className="d-flex flex-row justify-content-centers">
+            <Link
+              to="/"
+              className="btn btn-secondary ml-8"
+              type="button"
+              onClick={() => console.log('hello')}
+            >
+              Закрыть
+            </Link>
+            <button
+              className="btn btn-info"
+              type="button"
+              onClick={sendRequest}
+            >
+              Сохранить
+            </button>
           </div>
         </div>
       </form>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://localhost:3004';
+export const SERVICE_URL = `${BASE_URL}/service`;
+export const AUTHORS_URL = `${BASE_URL}/author`;
